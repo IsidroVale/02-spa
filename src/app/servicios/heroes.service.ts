bc7eb920040d837095b4155fcfe7348f523cf360
IsidroVale <isiperez1502@hotmail.com>
2019-04-21 16:55:16 -0400

    trabajo de estudio 1

---
+++ src/app/servicios/heroes.service.ts
@@ -0,0 +1,98 @@
+
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class HeroesService {
+
+    private heroes:Heroe[] = [
+        {
+            nombre: "Aquaman",
+            bio: "El poder más reconocido de aquaman es el capacidad telepática para comunicarse con la vida marina, la cual puede convocar a grandes distancias.",
+            img: "assets/img/aquaman.png",
+            aparicion: "1941-11-01",
+            casa: "DC"
+        },
+        {
+            nombre: "Batman",
+            bio: "Los rasgos principales de Batman se resumen en <<destreza fisica, habilidades deductivas y obsesión>>. la mayor parte de las caracteristicas básicas de los cómic han variado por las diferentes interpretaciones que le han dado al presonaje.",
+            img: "assets/img/batman.png",
+            aparicion: "1939-05-01",
+            casa: "DC"
+        },
+        {
+            nombre: "Daredevil",
+            bio: "Al haber perdido la vista, los cuatro sentidos restantes de Daredevil fueron aumentados por la radiación a niveles superhumanos, en el accidente que tuvo cuando era niño. A pesar de su ceguera, puede \"ver\" a través de un \"sexto sentido\" que le sirve como un radar similar al de los murciélagos.",
+            img: "assets/img/daredevil.png",
+            aparicion: "1964-01-01",
+            casa: "Marvel"
+        },
+        {
+            nombre: "Hulk",
+            bio: "Su principal poder es su capacidad de aumentar su fuerza hasta niveles prácticamente ilimitados a la vez que aumenta su furia. Dependiendo de que personalidad de hulk esté al mando en ese momento (el Hulk Banner es el más débil, pero lo compensa con su inteligencia).",
+            img: "assets/img/hulk.png",
+            aparicion: "1962-05-01",
+            casa: "Marvel"
+        },
+        {
+            nombre: "Linterna Verde", 
+            bio: "Poseedor del anillo de poder que posee la capacidad de crear marifestaciones de luz sólida mediante la utilización del pensamiento. Es alimentado por la Llama Verde(revisad por escritores posteriores como un poder místco llamado",
+            img: "assets/img/linterna-verde.png",
+            aparicion: "1940-06-01",
+            casa: "DC"
+        },
+        {
+            nombre: "Spider-Man",
+            bio: "Tras ser mordido por una araña radiactiva, obtuvo los siguientes poderes sobrehumanos, una gran fuerza, aglidad, poder trepar por paredes.la fuerza de Spider-Man le permite levantar 10 toneladas o más. Gracias a esta gran fuerza Spider-Man puede realizar saltos increibles. un \"Sentido arácnido\", que le permite saber si un peligro se cierne sobre él, antes de que suceda. En ocasiones este puede llevar a Spider-Man al origen del peligro",
+            img: "assets/img/spiderman.png",
+            aparicion: "1962-08-01",
+            casa: "Marvel"
+        },
+        {
+            nombre: "Wolverine",
+            bio: "En el universo ficticio de Marvel, Wolverine posee poderes regenerativos que puede curar cualquier herida, por mortal que esta sea, además ese msmo poder hace que sea inmune a cualquier enfermedad existente en la Tierra y algunas extraterrestres. Posee también una fuerza sobrehumana, que si bien no se compara con de otros superhéroes como Hulk, sí sobrepasa la de cualquier humano.",
+            img: "assets/img/wolverine.png",
+            aparicion: "1974-11-01",
+            casa: "Marvel"
+        }
+    ];
+
+    constructor() { 
+        console.log("Servicio listo para usar!!!");
+    }
+
+    getHeroes():Heroe[]{
+        return this.heroes;
+    }
+
+    getHeroe(idx: string){
+        return this.heroes[idx];
+    }
+
+    buscarHeroes( termino:string ):Heroe[]{
+
+        let heroesArr:Heroe[] = [];
+        termino = termino.toLowerCase();
+
+        for (let i = 0; i < this.heroes.length; i ++ ){
+
+            let heroe = this.heroes[i];
+
+            let nombre = heroe.nombre.toLowerCase();
+            if( nombre.indexOf( termino ) >=0 ){
+                heroe.idx = i;
+                heroesArr.push( heroe );
+            }
+        }
+        return  heroesArr;
+    }
+      
+}
+
+export interface Heroe{
+    nombre: string,
+    bio: string,
+    img: string,
+    aparicion: string;
+    casa: string;
+    idx?: number;
+}
